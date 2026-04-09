02833ddf923b0ae2cd4b103e3ff8858b6bc8d9c4
raya <raya.rumenova@backseatgaming.com>
2026-04-09 23:42:07 +0300

    database setup

--- app/app.py
+++ app/app.py
@@ -1,20 +1,45 @@
-from fastapi import FastAPI, HTTPException
-from schemas import PostCreate
+from fastapi import FastAPI, HTTPException, Form, File, UploadFile, Depends
+from app.schemas import PostCreate, PostResponse
+from app.db import Post, create_db_tables, get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 text_posts = {
     1: {"title": "First post", "content": "First post's content, not really much"},
-    2: {"title": "Second post", "content": "A:A;A::A:LALADPAda"},
-    3: {"title": "Third post", "content": "LSA:L:DL"}
+    2: {"title": "Second post", "content": "Second post, just a trial post"},
+    3: {"title": "Third post", "content": "Third post, now clear"}
 
 }
-@app.get("/hello")
-def hello_world():
-    return {"message": "Hello World"}
+
+@app.get("/posts/{id}")
+def get_post(id: int) -> PostResponse:
+    if id not in text_posts:
+        raise HTTPException(status_code=404, detail="Not found")
+    return text_posts.get(id)
 
 @app.post("/create")
-def create_post(post: PostCreate):
+def create_post(post: PostCreate) -> PostResponse :
     new_post = {"title": post.title, "content": post.content}
     text_posts[max(text_posts.keys()) + 1] = new_post
     return new_post
+
+@app.delete("/delete")
+def delete_post(id: int):
+    if id >= len(text_posts.keys()):
+        raise HTTPException(status_code=404, detail="Not found")
+    pass
+
+@app.upload("/upload")
+async def upload_post(
+        file: UploadFile = File(...),
+        caption: str = Form(...),
+        session: AsyncSession = Depends(get_async_session)
+):
+    pass
