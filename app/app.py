4a120ba863f335640a8f9c2f8bac0b8698658d3f
raya <raya.rumenova@backseatgaming.com>
2026-04-13 01:09:48 +0300

    imagekitio wiring

--- app/app.py
+++ app/app.py
@@ -4,6 +4,12 @@ from app.db import Post, create_db_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
 from sqlalchemy import select
+from app.images import imagekit
+from imagekitio.models.UploadFileRequestOptions import UploadFileRequestOptions
+import os
+import shutil
+import uuid
+import tempfile
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -20,20 +26,42 @@ text_posts = {
 }
 
 @app.post("/upload")
-async def upload_post(file: UploadFile = File(...),
+async def upload_file(file: UploadFile = File(...),
                       caption: str = Form(""),
                       session: AsyncSession = Depends(get_async_session)
 ):
-    post = Post(
-        caption=caption,
-        url="dummy url",
-        file_type="photo",
-        file_name="dummy name"
-    )
-    session.add(post)
-    await session.commit()
-    await session.refresh(post)
-    return post
+    temp_file_path = None
+
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splittext(file.filename)[1]) as tmp_dir:
+            temp_file_path = tmp_file.name
+            shutil.copyfileobj(file.file, temp_file)
+        upload_result = imagekit.upload_file(
+            file=open(temp_file_path, "rb"),
+            file_name=file.filename,
+            options=UploadFileRequestOptions(
+                use_unique_file_name=True,
+                tags=["backend_upload"]
+            )
+        )
+        if upload_result.response.http_status_code == 200:
+            post = Post(
+                caption=caption,
+                url="dummy url",
+                file_type="photo",
+                file_name="dummy name"
+            )
+            session.add(post)
+            await session.commit()
+            await session.refresh(post)
+            return post
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        if temp_file_path and os.path.exists(temp_file_path):
+            os.unlink(temp_file_path)
+        file.file.close()
 
 
 @app.get("/feed")
--- app/images.py
+++ app/images.py
@@ -6,6 +6,6 @@ load_dotenv()
 
 imagekit = ImageKit(
     private_key=os.getenv("IMAGEKIT_PRIVATE_KEY"),
-    public_key=os.getenv("IMAGEKIT_PUBLIC_KEY"),
-    url_endpoint=os.getenv("IMAGEKIT_URL_ENDPOINT"),
 )
+public_key = os.getenv("IMAGEKIT_PUBLIC_KEY")
+url_endpoint = os.getenv("IMAGEKIT_URL_ENDPOINT")
